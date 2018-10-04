e40c8e7354f03334412ebee22fe718bbabc3f155
ppaushya <43550473+ppaushya@users.noreply.github.com>
2018-10-04 18:42:56 +0530

    Add files via upload

--- BootClass.java
+++ BootClass.java
@@ -1,5 +1,7 @@
 import java.time.LocalDate;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class BootClass {
 	
@@ -8,8 +10,67 @@ public class BootClass {
 	public static Customer[] LoadCustomer()
 	{
 		
-		Customer[] customers=new Customer[5];
-		Address address=new Address("north avenue","dashmesh nagar","ludhiana","punjab");
+		Customer[] customer=new Customer[5];
+		
+		
+			
+			for(int i=0;i<5;i++)
+			{
+				Long customerId;
+				int accountNo,mobileNo;
+				String customerName,emailId;
+				customer[i]=new Customer();
+				customerId=scan.nextLong();
+				
+				Pattern pattern= Pattern.compile("^\\d{6,10}");
+				
+				boolean x= true;
+				while(x)
+				{
+					Matcher matcher= pattern.matcher(Long.toString(customerId));
+					if(matcher.matches())
+					{
+						System.out.println("Enter Customer Name");
+						x=false;
+					}
+					else
+					{
+						System.out.println("Enter a valid customer id");
+						customerId=scan.nextLong();
+					}
+				}
+				x=true;
+				customerName= scan.nextLine();
+				
+				Pattern pattern1= Pattern.compile("^[A-Z][A-Za-z\\s]+$");
+				
+				while(x)
+				{
+					Matcher matcher= pattern1.matcher(customerName);
+					if(matcher.matches())
+					{
+						System.out.println("Enter Customer Name");
+						x=false;
+					}
+					else
+					{
+						System.out.println("Enter a valid customer name");
+						customerName= scan.nextLine();
+					}
+				}
+				x=true;
+				
+				
+				
+				
+				
+					
+				 
+				
+			}
+			
+		
+		/*Address address=new Address("north avenue","dashmesh nagar","ludhiana","punjab");
 		customers[0] = new Customer(1100,"poonam",address,new Account[5],"9915155124","poonamojha96@gmail.com");
 		Account account=new Account(1234, AccountType.SAVINGS, LocalDate.now(), 45000);
 		customers[0].getAccount()[0]=account;
@@ -25,9 +86,9 @@ public class BootClass {
 		
 		Address address3=new Address("East Avennue", "Cross St", "Delhi", "UP");
 		customers[4]=new Customer(657657, "Jasmine",address3 , new Account[5], "352325", "jas@gmail.com");
+		*/
 		
-		
-		return customers;
+		return customer;
 	}
 	
 	public static void printCustomers()
@@ -40,6 +101,16 @@ public class BootClass {
 		}
 	}
 	
+	public static Customer findId(int id)
+	{
+		for(Customer customer:LoadCustomer())
+		{
+			if(customer.getCustomerId()==id)
+				return customer;
+		}
+		return null;
+	}
+	
 	
 
 	public static void main(String[] args) {
@@ -47,13 +118,35 @@ public class BootClass {
 		printCustomers();
 		
 		int id=0;
+		int choice=0;
 		System.out.println("Choose the customer id:");
 		id= scan.nextInt();
-		if()
+		Customer customer=findId(id);
+		char c=0;
+		c=scan.next().charAt(0);
 		
-		
+		do {
+				if(customer!=null)
+				{
+					System.out.println("1.Generate account\n2.Perform Transactions.");
+					System.out.println("Choose 1 or 2");
+					choice=scan.nextInt();
+					if(choice==1)
+					{
+						
+					}
+					else if(choice==2)
+					{
+						
+					}
+					else
+						System.out.println("Enter a valid option");
 	
-		
+				}
+				else
+					System.out.println("Print valid customer id");
+		}while(c=='y'||c=='Y');
+
 		
 
 	}
